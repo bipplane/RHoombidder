b31284337f22a2af6adc847aa8219ffbb2ee7218
Wei Rong <56021409+wr1159@users.noreply.github.com>
2022-10-01 10:38:18 +0000

    Add KillBot Feature

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ updater = Updater(token=os.environ['TOKEN'])
 dispatcher = updater.dispatcher
 
 def startCommand(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton("Tap to make money")], [KeyboardButton("Tap to do nothing")], [KeyboardButton("Tap to kick the bot")], [KeyboardButton("Tap to save a turtle")]]
+    buttons = [[KeyboardButton("Tap to make money")], [KeyboardButton("Tap to do nothing")], [KeyboardButton("Tap to kick the bot")], [KeyboardButton("PAY 9371 3379")]]
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(buttons))
+def endCommand(update: Update, context: CallbackContext):
+  
+  context.bot.send_message(chat_id=update.effective_chat.id, text="I am now useless", reply_markup = ReplyKeyboardRemove())
 
 dispatcher.add_handler(CommandHandler("start", startCommand))
+dispatcher.add_handler(CommandHandler("killthebot", endCommand))
 
 updater.start_polling()
+
